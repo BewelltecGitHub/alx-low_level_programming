ecf4d4732736d098baa0926ac08fb3297a1205f9
BEWELLTECNET <bewell.gh@gmail.com>
2023-04-27 21:34:24 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,9 +2,9 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list.
+ * print_list -  prints all the elements of a list_t list.
  * @h: pointer to the list_t list to print
- * Return: nodes printed
+ * Return: Always 0 (success)
  */
 size_t print_list(const list_t *h)
 {
